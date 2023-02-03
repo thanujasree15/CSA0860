0ed4c658473f95f00c45b57399e144d44fa815ec
thanujasree15 <122336057+thanujasree15@users.noreply.github.com>
2023-02-03 14:38:14 +0530

    Add files via upload

---
+++ 2.9 seasons.py
@@ -0,0 +1,20 @@
+month= input("Enter the month")
+date=int(input("Enter the date"))
+if month in("january","february","march"):
+    print("Summer season")
+elif month in ("April","may","june"):
+    print("Spring season")
+elif month in ("july","august","September"):
+    print("Autumn season")
+else:
+    print("winter season")
+
+if(month=="march") and (date>20):
+    print("Summerseason")
+elif(month=="june") and (date>21):
+    print("Spring season")
+elif(month=="september") and (date>22):
+    print("Autumn season")
+elif(month=="December") and (date>21):
+    print("winter season")
+
