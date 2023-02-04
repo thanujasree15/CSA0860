b4ea4eae93e3277670296982961f98da79c599c8
thanujasree15 <122336057+thanujasree15@users.noreply.github.com>
2023-02-04 14:55:46 +0530

    Add files via upload

---
+++ 3.8 grouping the anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+   def groupAnagrams(self, strs):
+      result = {}
+      for i in strs:
+         x = "".join(sorted(i))
+         if x in result:
+            result[x].append(i)
+         else:
+            result[x] = [i]
+      return list(result.values())
+ob1 = Solution()
+print(ob1.groupAnagrams(["ate","sat","tas","tea","eat","got","eye","yee"]))
