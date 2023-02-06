80d6409b821853ee37c9680dbe9abe01f7fcb01e
thanujasree15 <122336057+thanujasree15@users.noreply.github.com>
2023-02-06 13:27:55 +0530

    Add files via upload

---
+++ 4.10  Scramble String.py
@@ -0,0 +1,12 @@
+string=" i am great"
+words=string.split()
+list1=list(words)
+list2=[]
+for i in list1[: : -1]:
+    list2.append(i)
+print(list2)
+print("converting to string")
+strconv=""
+for x in list2:
+    strconv += '' +x
+print(strconv)
