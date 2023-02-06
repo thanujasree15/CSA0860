f4663c4ff973cb680a3c469237e98ad52c30f737
thanujasree15 <122336057+thanujasree15@users.noreply.github.com>
2023-02-06 13:23:50 +0530

    Add files via upload

---
+++ 4.5 max numbers of steps.py
@@ -0,0 +1,15 @@
+def minJumps(arr,l,h):
+    if(h==l):
+        return 0
+    if(arr[l]==0):
+        return float('inf')
+    min=float('inf')
+    for i in range(l+1,h+1):
+        if(i<l+arr[l]+1):
+            jumps=minJumps(arr,i,h)
+            if(jumps!=float('inf') and jumps+1<min):
+                min=jumps+1
+    return min
+arr=[5,5,6,8,4,5,7,8,9,1,2,4,5,6]
+n=len(arr)
+print('minimum number of jumps to reach','end is',minJumps(arr,0,n-1))
