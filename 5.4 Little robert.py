dead60040b74f16ae2d962aede49aaddfe321654
thanujasree15 <122336057+thanujasree15@users.noreply.github.com>
2023-02-07 09:39:56 +0530

    Update 5.4 Little robert.py

--- 5.4 Little robert.py
+++ 5.4 Little robert.py
@@ -1,17 +1,8 @@
-def palindrome(s):
-        i= 0
-        j=len(s)-1
-        while i < j:
-            while i < j and not s[i].isalnum():
-                i += 1
-            while i < j and not s[j].isalnum():
-                j -= 1
-            if s[i].lower() != s[j].lower():
-                return False
-            i, j = i + 1, j - 1
-        return True
-print(palindrome("race a car"))
-print(palindrome(""))
-print(palindrome("madam"))
-print(palindrome("honest"))
-print(palindrome("A man,a plan,a canal:Panama"))
+a=int(input("Enter a number :")) #float
+b=int(input("Enter a second number :")) #flaot
+c=a*b
+d=a+b
+if(c%d==0):
+    print("YEAH")
+else:
+    print("NAH")
